dd6027e186dba999c36202e95f212cfd1777095f
Alexander Ivanishko <ivanishco@mail.ru>
2018-12-12 18:13:50 +0300

    enable gii

--- backend/views/posts/userposts.php
+++ backend/views/posts/userposts.php
@@ -2,7 +2,7 @@
 use yii\helpers\Html;
 use yii\widgets\LinkPager;
 ?>
-<h1>Ваши записи</h1>
+<h1>Ваши записи (<?= Yii::$app->user->identity->username; ?>)</h1>
 <ul>
 <?php foreach ($posts as $post): ?>
     <li>
