b87b6ff0167b3b9e14a4948911b0822b488a1f33
Alexander Ivanishko <ivanishco@mail.ru>
2018-12-12 09:29:50 +0300

    regular expression

--- frontend/models/EntryForm.php
+++ frontend/models/EntryForm.php
@@ -27,8 +27,8 @@ class EntryForm extends ActiveRecord
 		return [
 			['name', 'string', 'min' => 3],
 			['sname', 'string', 'min' => 3],
-			['name', 'match', 'pattern' => '/^[А-яёЁA-z]+$/', 'message' => 'В имени только буквы!'], 
-			['sname', 'match', 'pattern' => '/^[а-яА-ЯёЁa-zA-Z]+$/', 'message' => 'В фамилии только буквы!'], 	
+			['name', 'match', 'pattern' => '/^[а-яА-ЯЁёA-Za-z]+$/u', 'message' => 'В имени только буквы!'], 
+			['sname', 'match', 'pattern' => '/^[а-яА-ЯЁёA-Za-z]+$/u', 'message' => 'В фамилии только буквы!'], 	
 			[['name','sname','phone','email','textarea'], 'required'],
 			['email', 'email'],
 			['text_id', 'safe' ],
